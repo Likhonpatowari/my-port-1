f623db58873292db80c88b579d6009e55a62b93d
Likhon <likhonpatowari2@gmail.com>
2023-07-07 12:31:12 +0600

    4th commit

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/Likhonpatowari/my-port-1.git
 git push -u origin main
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,10 @@
+// ---- Tying Animation Start 
+let typed = new Typed('.auto-input', {
+    strings:['Fornt-End Developer','Freelancer','UI Designer','Youtuber'],
+    typeSpeed: 100,
+    backspeed: 20000,
+    backDelay: 1000,
+    loop: true,
+ });
+ 
+// ---- Tying Animation End 
